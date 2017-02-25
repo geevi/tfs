ff4641371047b731e7ec0b4ff2c33d503b3228da
Girish Varma <girishrv@gmail.com>
2017-02-25 15:07:36 +0530

    tested on cifar

--- tfs.py
+++ tfs.py
@@ -1,12 +1,10 @@
-import tensorflow 
-import numpy 
+import tensorflow as tf
+import numpy as np
 import os
-from tensorflow.examples.tutorials.mnist import input_data
 import time
 
-time    = time
-tf      = tensorflow
-np      = numpy
+from layers import *
+
 flags   = tf.app.flags
 FLAGS   = flags.FLAGS
 logging = tf.logging
@@ -18,19 +16,46 @@ flags.DEFINE_string("working_directory", "", "")
 flags.DEFINE_integer("B", 100, "batch size")
 
 
-
 allow_soft_placement    = True
 log_device_placement    = False
 
 
-def mnist():
-    data_directory = os.path.join(FLAGS.working_directory, "MNIST")
-    if not os.path.exists(data_directory):
-        os.makedirs(data_directory)
-    mnist = input_data.read_data_sets(data_directory, one_hot=True)
-    return mnist
+def train(ctrl, model):
+
+    step = 0
+    try:
+        start = time.time()
+        while not ctrl['coord'].should_stop():
+            step += FLAGS.B
+
+            summ = model.train(ctrl['sess'])
+
+            ctrl['writer_train'].add_summary(summ, step)
+            if step % 10000 == 0:
+                # print 'Epoch ', step /1000
+                summ = model.test(ctrl['sess'])
+                ctrl['writer_test'].add_summary(summ, step)
+                ctrl['writer_train'].flush()
+                ctrl['writer_test'].flush()
+
+                # print y_undec_[:,0]
+                # print y_gt_[:,0]
+                end     = time.time() - start
+                print 'time for 10000 images ', end
+                start   = time.time()
+
+    except tf.errors.OutOfRangeError:
+
+        print 'Training done'
+        # saver.save(sess, FLAGS.name + "/model", global_step = step)
+
+    finally:
+        ctrl['coord'].request_stop()
+        # saver.save(sess, FLAGS.name + "/model", global_step = step)
 
 
+    ctrl['coord'].join(ctrl['threads'])
+    ctrl['sess'].close()
 
 def init():
     if not os.path.exists('saved/' + FLAGS.name):
@@ -68,8 +93,6 @@ def match(y, y_pred, name = 'match'):
 def softmax_cross_entropy(y, y_pred, name = 'softmax_cross_entropy'):
     return tf.losses.softmax_cross_entropy(y, y_pred)
 
-
-
 losses = {
     'softmax_cross_entropy': softmax_cross_entropy
 }
@@ -78,17 +101,28 @@ accuracies = {
     'class_match' : match
 }
 
-def classify(y, y_pred, **kwargs):
+def classify(y, y_pred, y_test = None, y_pred_test = None, **kwargs):
     loss = kwargs.get('loss', 'softmax_cross_entropy')
-    loss = losses[loss](y, y_pred)
-    tf.summary.scalar('loss', loss)
     acc  = kwargs.get('acc', 'class_match')
-    acc  = accuracies[acc](y, y_pred)
-    tf.summary.scalar('accuracy', acc)
+    train_loss = losses[loss](y, y_pred)
+    train_acc  = accuracies[acc](y, y_pred)
+    
+    train_summary = [
+        tf.summary.scalar('loss', train_loss),
+        tf.summary.scalar('accuracy', train_acc)
+    ]
+
+    if y_test != None:
+        test_loss = losses[loss](y_test, y_pred_test)
+        test_acc  = accuracies[acc](y_test, y_pred_test)
+        test_summary = [
+            tf.summary.scalar('loss', test_loss),
+            tf.summary.scalar('accuracy', test_acc)
+        ]
+
     optimizer = kwargs.get('optimizer', 'adam')
-    optimizer = minimize(loss, rate = kwargs.get('rate', 0.001), algo = optimizer)
-    summary = tf.summary.merge_all()
-    return optimizer, summary
+    optimizer = minimize(train_loss, rate = kwargs.get('rate', 0.001), algo = optimizer)
+    return optimizer, train_summary, test_summary
 
 
 def minimize(loss, **kwargs):
@@ -102,94 +136,6 @@ def minimize(loss, **kwargs):
             return tf.train.GradientDescentOptimizer(learning_rate=rate).minimize(loss)
         elif algo == 'momentum':
             return tf.train.MomentumOptimizer(rate, kwargs.get('momentum', None)).minimize(loss)
-
-
-def _convs(y, lname, ltype, lcfg):
-    chan    = lcfg.get('chan', y.get_shape().as_list()[-1])
-    window  = lcfg.get('window', 3)
-    bias    = lcfg.get('bias', True)
-    act     = lcfg.get('act', None)
-    bn      = lcfg.get('batch_norm', False)
-    stride  = lcfg.get('stride', 1)
-    padding = lcfg.get('padding', 'same')
-    
-    if ltype == 'deconv':
-        y = tf.layers.conv2d_transpose(y, chan, window, strides= stride, padding = padding, use_bias = bias, name = lname)
-    elif ltype == 'conv':
-        y = tf.layers.conv2d(y, chan, window, strides= stride, padding = padding, use_bias = bias, name = lname)
-    if bn:
-        momentum = lcfg.get('batch_norm_momentum', 0.9)
-        training = lcfg.get('training', True)
-        y = tf.contrib.layers.batch_norm(y, is_training=training, scale=True, updates_collections=None, decay=momentum)
-        # y = tf.layers.batch_normalization(y , axis = 3, name = lname + '_batch_norm', training = training, momentum = momentum)
-
-    if act:
-        y = activation(y, name = lname + '_' + act, type = act, alpha = lcfg.get('alpha', None))
-
-    return y
-
-
-
-def activation(x, name, **kwargs):
-    atype = kwargs.get('type', 'relu')
-    if atype == 'relu':
-        y = tf.nn.relu(x, name = name)
-    elif atype == 'tanh':
-        y = tf.nn.tanh(x, name = name )
-    elif atype == 'leaky_relu':
-        alpha = kwargs.get('alpha', 0.)
-        y = tf.maximum(alpha*x, x, name = name)
-    elif atype == 'sigmoid':
-        y = tf.nn.sigmoid( x, name = name )
-    return y
-
-def reshape(x, name, **kwargs):
-    shape    = kwargs.get('shape', kwargs.get('shape'))
-    if shape == None:
-        shape = [-1, reduce(lambda a,b: a*b, x.get_shape().as_list()[1:])]
-    return tf.reshape(x, shape, name = name)
-
-def deconv(x, name, **kwargs):
-    return _convs(x, name, 'dconv', kwargs)
-
-def conv(x, name, **kwargs):
-    return _convs(x, name, 'conv', kwargs)
-
-def dropout(x, name, **kwargs):
-    pr = kwargs.get('pr', 0.5)
-    return tf.layers.dropout(y, pr, name = name)
-
-def pool(x, name, **kwargs):
-    window  = kwargs.get('window', 2)
-    stride  = kwargs.get('stride', 2)
-    padding = kwargs.get('padding', 'same')
-    return tf.layers.max_pooling2d(x, window, stride, padding, name = name)
-
-def dense(x, name, **kwargs):
-    units = kwargs.get('units', None)
-    act = kwargs.get('act', None)
-    y =  tf.layers.dense(x, units, name = name)
-    if act:
-        y = activation(y, name+ '_' + act, type =act, alpha = kwargs.get('alpha', None))
-    return y
-
-def normalize(x, name = 'normalize', **kwargs):
-    mean = kwargs.get('mean', 0)
-    variance = kwargs.get('variance', 1)
-    with tf.name_scope(name):
-        y = (x - mean) / variance
-        return y
-
-
-predefined_layers = {
-    'reshape'   : reshape,
-    'conv'      : conv,
-    'deconv'    : deconv,
-    'dropout'   : dropout,
-    'pool'      : pool,
-    'dense'     : dense,
-    'normalize' : normalize
-}
 
 def sequential(x, net, defaults = {}, name = '', reuse = None, var = {}, layers = {}):
     layers = dict(layers.items() + predefined_layers.items())
